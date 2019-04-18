9ce5a7fac50bd3a51b331435977c06f8e2875892
Kukel Attila <kukel.attila@different.hu>
2019-04-18 16:52:08 +0200

    bourne shell script upgrade

--- app/Libraries/ping.sh
+++ app/Libraries/ping.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-arg1="${1:-}"
+#####################
+# cd /path/to/system/dir && sh app/Libraries/ping.sh IP_ADDRESS_1 IP_ADDRESS_2 IP_ADDRESS_N
+#####################
 
 # set script constants
 PING_WHICH=/bin/ping
-PING_SERVER="8.8.8.8"
-PING_LOCAL="192.168.0.1"
 PHP_WHICH=/usr/bin/php
-TEMP_PATH=/opt/xampp/htdocs/ns/app/Libraries/ping_data/
+TEMP_PATH=app/Libraries/ping_data/
 TEMP_PING_NAME=ping.txt
+
 # set nsetspead constants
-NS_PATH=/opt/xampp/htdocs/ns/
 NS_COMMAND="store:ping"
 
-${PING_WHICH} ${PING_SERVER} -fqc 10 > ${TEMP_PATH}${TEMP_PING_NAME}
-${PHP_WHICH} ${NS_PATH}artisan ${NS_COMMAND} ${TEMP_PATH}${TEMP_PING_NAME}
-rm ${TEMP_PATH}${TEMP_PING_NAME}
-
-${PING_WHICH} ${PING_LOCAL} -fqc 10 > ${TEMP_PATH}${TEMP_PING_NAME}
-${PHP_WHICH} ${NS_PATH}artisan ${NS_COMMAND} ${TEMP_PATH}${TEMP_PING_NAME}
-rm ${TEMP_PATH}${TEMP_PING_NAME}
+for HOST in "$@"
+do
+    ${PING_WHICH} ${HOST} -fqc 10 > ${TEMP_PATH}${TEMP_PING_NAME}
+    ${PHP_WHICH} artisan ${NS_COMMAND} ${TEMP_PATH}${TEMP_PING_NAME}
+    rm ${TEMP_PATH}${TEMP_PING_NAME}
+done
